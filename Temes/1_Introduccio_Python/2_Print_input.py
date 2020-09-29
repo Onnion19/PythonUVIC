1493bafe76d9cdcc9c2124923ff45faf1df905bc
Aleix Rius <u1940025@campus.udg.edu>
2020-09-29 15:55:21 +0200

    Added input info

--- Temes/1_Introduccio_Python/2_Print_input.py
+++ Temes/1_Introduccio_Python/2_Print_input.py
@@ -30,3 +30,21 @@ print("--------------------------------------------") #ignorar aquesta linia
 
 print (str(x) + z); #converteix 3 en "3", aixi que el resultat de la operacio es "3Hola"
 
+
+#Llegir entrada per teclat 
+"""
+Podem llegir dades des de fora del programa, que ens entri el usuari.
+"""
+print("Com et dius?")
+nom = input("\t Entra el teu nom:"); 
+print("Aixi que et dius " + nom); 
+
+edat = input("\t Quina edat tens?"); #WARNING!" Encara que entrem un numero, es detecta com si fos text.
+print("Que gran que ets amb " + edat + " anys"); 
+
+
+"""
+Com podem llegir un numero? i que no es transformi amb text?
+""" 
+edat = eval(input("Entra la teva edat")); #Utiltizar eval al principi ens retorna el tipus de dada que creu mes convenient. En aquest cas un numero (si li entrem un numero esclar) 
+print("Que gran que ets amb " + edat + " anys"); #Ens dona error perque no pot fer la suma de text + numero + text
