569318775e7f656e983ca63fbe58fa098e10c5db
Onnion191 <aleixrius19@gmail.com>
2020-12-04 16:38:39 +0100

    Examen 2

---
+++ Temes/3_Estructures_Basiques/Exercicis/Examens/Examen2.py
@@ -0,0 +1,9 @@
+'''
+Donada una cadena de bases (A, T, C, G), calcular el percentatge de T que es troben
+després d’una A i el percentatge de G que es troben després d’una C .
+Exemple: ATTCGCGAGCTATACCCTGCGA
+% de T després d’A : 40
+% de G després de C : 60
+'''
+
+
