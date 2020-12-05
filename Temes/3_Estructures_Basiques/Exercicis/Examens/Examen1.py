4e13e945ce1b3026a1db0e7453fd25dd4529c330
Onnion191 <aleixrius19@gmail.com>
2020-12-05 10:46:51 +0100

    Examen1

---
+++ Temes/3_Estructures_Basiques/Exercicis/Examens/Examen1.py
@@ -0,0 +1,43 @@
+'''
+Ens entraran dues seqüències de valors separats per espai. La primera conté les bases i
+la segona la quantitat que s’ha trobat de cada una d’elles en un tros de DNA avaluat.
+Es demana fer un programa en PYTHON que ens retorni quina és la moda de la mostra
+i ens digui si és asimètrica a l’esquerra o a la dreta, tenint en compte si hi ha més bases
+trobades a l’esquerra o a la dreta de la moda o bé si és simètrica si en té la mateixa
+quantitat.
+En cas de que hi hagi més d’una base que tingui la mateixa moda, no ha de dir si és
+asimètrica o no, sinó que cal que mostri quines són aquestes bases.
+Si la quantitat de valors no és igual a les dues seqüències cal que es mostri missatge
+d’error.
+
+Exemple1 : 				Exemple2:
+Cadena1= A C G T 		Cadena1= A C G T
+Cadena2= 18 10 15 30 	cadena2= 10 121 71 100
+
+Resposta: 				Resposta:
+Moda: T 				Moda: C
+Asimètrica a l’esquerra Asimètrica a la dreta
+'''
+
+Cadena1 = "A C G T"
+Cadena1.upper(); 
+Cadena2 = "18 10 15 30"
+
+Bases = Cadena1.split(); 
+Ocurrencies = Cadena2.split();
+
+index = 0; 
+max = 0;
+
+#Trobem la moda
+for i in range(len(Ocurrencies)): # range [0, ultim)	
+	ocurrencia = int(Ocurrencies[i]);
+	if( ocurrencia > max): 
+		max = ocurrencia; 
+		index = i;
+
+Moda = Bases[index];
+print(f'La moda es = {Moda}')
+
+
+
