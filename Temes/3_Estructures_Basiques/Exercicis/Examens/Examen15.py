b869f6cad4bdd17cf782585354a489c85409695d
Onnion191 <aleixrius19@gmail.com>
2021-01-13 18:27:18 +0100

    15

---
+++ Temes/3_Estructures_Basiques/Exercicis/Examens/Examen15.py
@@ -0,0 +1,24 @@
+''' 
+ 
+ 1 - Tenim dos fitxer "Compost.txt" i "Reaccions.txt" 
+	on el compost.txt i cal construir-ne un dicconari- 
+	
+		clau 			Valor
+	FormulaOrganica   nom comu 
+	
+	Al fitxer "reaccions:" 
+	
+	FromulaOrganica gr_compost   gC02alliberats   gH20alliberada
+		C2H2 			100g		 230g				70g 	
+
+2- Llegir el fitxer compost i crear el diccionari apropiat 
+3- L'usuari ha d'introduir els grams que vol reaccionar 
+4- Cal trobat quin es el compost que genera menys CO2 
+	amb els grams introduits per l'usuari
+'''
+
+##Part 1:
+def LlegirFitxerIPassarLlista(nomFitxer): 
+	##Llegira un fitxer i passara el seu contingut a una llista
+	
+def CrearDiccionariCompostos(nomFitxer): 
